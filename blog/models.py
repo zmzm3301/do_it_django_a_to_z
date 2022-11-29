48e36d85766dde1fb2c534ba411a36bade687180
beansdrawer <beansdrawer@naver.com>
2022-11-29 14:37:49 +0900

    Post Model Set

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author : 외래키 적용 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
+
