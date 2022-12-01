5f32068e3a1df97a7a01ec516d6ff14429a1504b
beansdrawer <beansdrawer@naver.com>
2022-12-01 14:09:22 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -6,14 +6,32 @@ class TestView(TestCase) :
     def setUp(self) :
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do it Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Blog')
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
@@ -48,9 +66,7 @@ class TestView(TestCase) :
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
         self.assertIn(post_001.title, soup.title.text)
 
