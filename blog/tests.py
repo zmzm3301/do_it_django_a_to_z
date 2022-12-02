385f844bc8f63905eb76a2d7b3ee5818aa3d4931
YeongMin Cho <zmzm3301@naver.com>
2022-12-02 10:58:55 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -99,23 +99,21 @@ class TestView(TestCase) :
 
 
     def test_post_detail(self):
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='TDD... 맘에 안 드는 단어야',
-            author=self.user_trump
-        )
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
+
         self.assertIn(self.user_trump.username.upper(), post_area.text)
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
