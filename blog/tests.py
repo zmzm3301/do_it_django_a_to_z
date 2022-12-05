a168d73dc77af159c68a79a3c7f348164b98fac7
YeongMin Cho <zmzm3301@naver.com>
2022-12-05 14:10:12 +0900

    포스트 수정할 때 텍스트로 태그 지정하기

--- blog/tests.py
+++ blog/tests.py
@@ -213,10 +213,10 @@ class TestView(TestCase):
 
     def test_update_post(self):
         update_post_url = f'/blog/update_post/{self.post_003.pk}/'
-
+        # 로그인을 안했을 경우
         response = self.client.get(update_post_url)
         self.assertNotEqual(response.status_code, 200)
-
+        # 로그인 했지만 작성자가 아닌 경우
         self.assertNotEqual(self.post_003.author, self.user_trump)
         self.client.login(
             username=self.user_trump.username,
@@ -237,12 +237,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세 번째 포스트를 수정했습니다.',
                 'content': '안녕? 세계, 우리는 하나!',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
@@ -251,4 +256,8 @@ class TestView(TestCase):
         self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
         self.assertIn('안녕? 세계, 우리는 하나!', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
 
