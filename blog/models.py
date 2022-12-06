507aa75b4172db6138fee9b0e6ebf84d9e739250
YeongMin Cho <zmzm3301@naver.com>
2022-12-06 15:54:54 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -78,3 +78,10 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1365/5e40c990116f75d0/svg/{self.author.email}'
+
