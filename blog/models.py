02792aae523e1d237dc4bbe175a7a12986a860c3
YeongMin Cho <zmzm3301@naver.com>
2022-12-06 16:46:09 +0900

    대문 페이지 수정

--- blog/models.py
+++ blog/models.py
@@ -66,6 +66,13 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/1365/5e40c990116f75d0/svg/{self.author.email}'
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
